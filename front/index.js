c8b94681ca13c75417a1fbee3f9ebcc38b0ef1ff
Natalya Babenko <ifelseiforelif@gmail.com>
2025-11-20 18:47:56 +0200

    added POST, DELETE

--- front/index.js
+++ front/index.js
@@ -1,12 +1,46 @@
 const URL = "http://localhost:3000/todos";
 //METHOD GET
+//REST API
+/*
+GET - Отримання даних
+POST - Додавання даних
+DELETE - видалення даних
+PUT - редагування
+*/
 async function getTodos() {
   try {
     const response = await fetch(URL);
     const todos = await response.json();
     for (let i = 0; i < todos.length; i++) {
-      console.log(todos[i].title);
+      console.log(todos[i]);
     }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addTodo(todo) {
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function deleteTodo(id) {
+  try {
+    const response = await fetch(`${URL}/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
   } catch (err) {
     console.log(err);
   }
@@ -15,4 +49,21 @@ async function getTodos() {
 const btn = document.getElementById("todoBtn");
 btn.addEventListener("click", () => {
   getTodos();
+});
+
+const addTodoBtn = document.getElementById("addTodoBtn");
+addTodoBtn.addEventListener("click", () => {
+  const todo = {
+    id: "31",
+    title: "my title",
+    comments: "нова задача",
+    status: "done",
+  };
+  addTodo(todo);
+});
+
+const deleteTodoBtn = document.getElementById("deleteTodo");
+deleteTodoBtn.addEventListener("click", () => {
+  const id = document.getElementById("id_todo").value;
+  deleteTodo(id);
 });
